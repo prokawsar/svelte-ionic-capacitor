066793e7626a87101bf5eef6a5bef9ef62515a2e
Kawsar Ahmed <prokawsar@gmail.com>
2022-06-25 23:29:21 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Router } from "@roxi/routify";
+	import { routes } from "../.routify/routes";
+
+	// import "@ionic/core/dist/ionic/index.esm.js";
+	import "@ionic/core/dist/ionic/ionic.js";
+	import "@ionic/core/css/ionic.bundle.css";
+
+	
+</script>
+
+<Router {routes} />
+
+<style @global>
+
+</style>
---
+++ src/pages/index.svelte
@@ -0,0 +1,39 @@
+<script>
+    // native API Toast
+    import { Toast } from '@capacitor/toast';
+    var name = "svelte with ionic with capacitor"
+    var image = "svelte_ionic_cap.png"
+    
+    const showHelloToast = async () => {
+        await Toast.show({
+            text: 'Test Toast!',
+        });
+    };
+
+</script>
+
+<div class="main">
+    <img src={image} alt=""/>
+    <h1>Welcome to {name}!</h1>
+
+    
+    <ion-grid>
+        <ion-row>
+            <ion-col>
+                <ion-button color="warning" on:click={showHelloToast}>Show Toast</ion-button>    
+            </ion-col>
+        </ion-row>
+    </ion-grid>
+
+</div>
+
+<style>
+    .main {
+        text-align: center;
+        margin: 50px auto;
+    }
+
+    img {
+        max-width: 100%;
+    }
+</style>
